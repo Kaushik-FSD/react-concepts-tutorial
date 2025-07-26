494643c38c1251e5610ace09521ca1f8e57de31f
KAUSHIK MANDAL <rockstarkaushik1000@gmail.com>
2025-07-26 11:39:14 +0530

    folder rename

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import FetchApiCall from "./react-tutorial/fetch-api-and-use-effect/FetchApiCall";
-import UseStateHookExample from "./react-tutorial/react-hooks/UseStateHookExample";
+import UseStateHookExample from "./react-tutorial/react-use-state/UseStateHookExample";
 import ReactRouterTutorial from "./react-tutorial/react-router/ReactRouterTutorial";
 import StateManagementHook from "./react-tutorial/state-management-use-context/StateManagementHook";
 import ToDo from "./react-tutorial/to-do-app/ToDo";
