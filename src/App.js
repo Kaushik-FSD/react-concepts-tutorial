a6e86b3233c292f24fae0aca1e1aa0d59c261187
KAUSHIK MANDAL <rockstarkaushik1000@gmail.com>
2025-07-26 10:55:59 +0530

    react router dom tutorial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import FetchApiCall from "./react-tutorial/fetch-api-and-use-effect/FetchApiCall";
 import UseStateHookExample from "./react-tutorial/react-hooks/UseStateHookExample";
+import ReactRouterTutorial from "./react-tutorial/react-router/ReactRouterTutorial";
 import ToDo from "./react-tutorial/to-do-app/ToDo";
 // import ComponentPropsExample from "./react-tutorial/components-props/ComponentPropsExample";
 // import TerniaryOperation from "./react-tutorial/terniary-operation/TerniaryOperation";
@@ -11,7 +12,8 @@ function App() {
       <TerniaryOperation /> */}
       {/* <UseStateHookExample /> */}
       {/* <ToDo /> */}
-      <FetchApiCall />
+      {/* <FetchApiCall /> */}
+      <ReactRouterTutorial />
     </div>
   );
 }
---
+++ src/react-tutorial/react-router/ReactRouterTutorial.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "../../pages/Home";
+import Menu from "../../pages/Menu";
+function ReactRouterTutorial() {
+  return (
+    <div>
+      <Router>
+        {/* Anything defines under Router will be accisable for the react-router-dom,
+        but does not mean that it has to be a route.
+        The links stays static in the page (but still accessing the react-router-dom functionalities
+        to change the routes, but anything defines under Routes, will be served as a new component aka pages) */}
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+        </div>
+
+        {/* Routes is nothing but to define the route inside it, 
+        any route defined inside Routes will be served as a page/component */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="*" element={<h1>Error Page: 404 Not Found</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default ReactRouterTutorial;
